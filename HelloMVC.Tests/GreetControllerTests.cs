bd58288429ad1abd2075513620d63d01fae1600a
Taylor Hemphill <hemphilltaylor@gmail.com>
2019-10-01 15:05:10 -0400

    AnotherControllerGreet

---
+++ HelloMVC.Tests/GreetControllerTests.cs
@@ -0,0 +1,23 @@
+﻿using HelloMVC.Controllers;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace HelloMVC.Tests
+{
+    public class GreetControllerTests
+    {
+        [Fact]
+        public void Index_Returns_ViewResult()
+        {
+        var controller = new GreetController();
+
+        var result = controller.Index("ThisIsAString");
+
+        Assert.IsType<ViewResult>(result);
+
+        }
+    }
+}
